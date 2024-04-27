7a292fb311b72529fbb6a98a6e53829eab4d0ce7
who-is-ai <149604746+who-is-ai@users.noreply.github.com>
2024-04-27 16:09:43 +0500

    Delete fqwf directory

--- fqwf/appModules/api/index.js
+++
@@ -1,8 +0,0 @@
-const {getData} = require('./api-utils.js'); 
-const {getRandomGame} = require('./api-utils.js'); 
-const endpoints = require('./config.js'); 
-module.exports={
-    getData,
-    getRandomGame,
-    endpoints
-}
